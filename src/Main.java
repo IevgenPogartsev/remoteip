37896e4e016bd941a60c4ffbb121976190114452
Ievgen Pogartsev <ipogartsev@luxoft.com>
2019-07-08 14:11:21 +0300

    M3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("M-1");
+        System.out.println("M-2");
+        System.out.println("M-3");
+
     }
 }
