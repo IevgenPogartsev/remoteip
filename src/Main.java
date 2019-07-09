362bbfa6124b9d28c187c0bd4a8c27ebf2eef80d
Ievgen Pogartsev <ipogartsev@luxoft.com>
2019-07-09 13:15:08 +0300

    M-5

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("M-2");
         System.out.println("M-3");
         System.out.println("M-4");
+        System.out.println("M-5-to create conflict");
 
     }
 }
