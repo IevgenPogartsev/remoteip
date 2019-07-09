876021c7f799e37fe5bd1eaa8fa0046ea1aae772
Ievgen Pogartsev <ipogartsev@luxoft.com>
2019-07-09 13:16:02 +0300

    only brach record

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("M-2");
         System.out.println("M-3");
         System.out.println("M-4");
-        System.out.println("M-5-to create conflict");
+        System.out.println("M-5-to create conflict-only in branch");
 
     }
 }
